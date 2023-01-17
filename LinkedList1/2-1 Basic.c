6d9ba14015638428d87e97fcc8b71bc6aebf8c05
tingmon <Tjeong2320@conestogac.on.ca>
2023-01-17 01:40:36 -0500

    Double Linked List Working

--- LinkedList1/2-1 Basic.c
+++ LinkedList1/2-1 Basic.c
@@ -15,19 +15,43 @@ int g_nSize;
 
 void InitList()
 {
+	// memory allocation
 	g_pHead = (NODE*)malloc(sizeof(NODE));
 	g_pTail = (NODE*)malloc(sizeof(NODE));
 	g_nSize = 0;
+
+	// memory initialization
+	memset(g_pHead, 0, sizeof(NODE));
+	memset(g_pTail, 0, sizeof(NODE));
+
+	g_pHead->next = g_pTail;
+	g_pTail->prev = g_pHead;
 }
 
 void DropList()
 {
-
+	printf("g_nSize[%d], g_pHead[%p], g_pTail[%p]\n", g_nSize, g_pHead, g_pTail);
+	NODE* pTemp = g_pHead;
+	while (pTemp->next != g_pTail) {
+		NODE* pDelNode = pTemp;
+		pTemp = pTemp->next;
+		free(pDelNode);
+	}
 }
 
 void PrintList()
 {
-
+	int counter = 0;
+	NODE* pTemp = g_pHead;
+	while (pTemp != NULL) {
+		printf("prev: [%p] | data: %s | next: [%p]\n", pTemp->prev, pTemp->pData, pTemp->next);
+		pTemp = pTemp->next;
+		counter++;
+	}
+	if (counter == 0) 
+	{
+		printf("List is Empty.\n");
+	}
 }
 
 int GetLength()
@@ -35,24 +59,127 @@ int GetLength()
 	return g_nSize;
 }
 
-void IsEmpty()
+int IsEmpty()
 {
-
+	if (g_pHead->next == g_pTail)
+		return 1;
+	return 0;
 }
 
 int InsertHead(const char* data)
 {
-	return 0;
+	// allocation, initialization, substitution
+	NODE* pNewNode = malloc(sizeof(NODE));
+	memset(pNewNode, 0, sizeof(NODE));
+	strcpy_s(pNewNode->pData, sizeof(NODE), data);
+	//if (g_nSize == 0) 
+	//{
+	//	g_pTail->prev = pNewNode;
+	//}
+	//else 
+	//{
+	//	NODE* pTemp = g_pHead->next;
+	//	pTemp->prev = pNewNode;
+	//}
+	pNewNode->prev = g_pHead;
+	pNewNode->next = g_pHead->next;
+	g_pHead->next = pNewNode;
+	pNewNode->next->prev = pNewNode;
+
+	g_nSize++;
+
+	return g_nSize;
 }
 
 int InsertTail(const char* data)
 {
-	return 0;
+	// allocation, initialization, substitution
+	NODE* pNewNode = malloc(sizeof(NODE));
+	memset(pNewNode, 0, sizeof(NODE));
+	strcpy_s(pNewNode->pData, sizeof(NODE), data);
+	//if (g_nSize == 0)
+	//{
+	//	g_pHead->next = pNewNode;
+	//}
+	//else
+	//{
+	//	NODE* pTemp = g_pTail->prev;
+	//	pTemp->next = pNewNode;
+	//}
+	pNewNode->prev = g_pTail->prev;
+	pNewNode->next = g_pTail;
+	g_pTail->prev = pNewNode;
+	pNewNode->prev->next = pNewNode;
+
+	g_nSize++;
+
+	return g_nSize;
 }
 
-int InsertMiddle(const char* data)
+int InsertMiddle(const int index, const char* data)
 {
-	return 0;
+	if (GetLength() == 0 || index > GetLength() || index < 0)
+		return 0;
+	// allocation, initialization, substitution
+	NODE* pNewNode = malloc(sizeof(NODE));
+	memset(pNewNode, 0, sizeof(NODE));
+	strcpy_s(pNewNode->pData, sizeof(NODE), data);
+
+	NODE* front = GetNode(index - 1);
+	NODE* rear = GetNode(index);
+
+	pNewNode->prev = front;
+	pNewNode->next = rear;
+	front->next = pNewNode;
+	rear->prev = pNewNode;
+
+	g_nSize++;
+
+	return g_nSize;
+}
+
+NODE* GetNode(const int index)
+{
+	int length = GetLength();
+	int counter = 0;
+	if (index > length || index < 0)
+		return NULL;
+	if (length - index > length / 2)
+	{
+		if (index == 0)
+		{
+			return g_pHead;
+		}
+
+		NODE* pTemp = g_pHead->next;
+		while (pTemp != NULL)
+		{
+			if (counter == index)
+			{
+				return pTemp;
+			}
+			pTemp = pTemp->next;
+			counter++;
+		}
+	}
+	else 
+	{
+		if (length - index == 0)
+		{
+			return g_pTail;
+		}
+		NODE* pTemp = g_pTail->prev;
+		while (pTemp != NULL)
+		{
+			if (counter == length - index)
+			{
+				return pTemp;
+			}
+			pTemp = pTemp->prev;
+			counter++;
+		}
+	}
+	return NULL;
 }
 
 NODE* GetNode(const char* data)
@@ -68,5 +195,26 @@ int DeleteNode(const char* data)
 
 int main(void) 
 {
+	InitList();
+
+	InsertHead("TestHead1");
+	InsertHead("TestHead2");
+	InsertHead("TestHead3");
+
+	PrintList();
+
+	InsertTail("TestTail1");
+	InsertTail("TestTail2");
+	InsertTail("TestTail3");
+
+	PrintList();
+
+	InsertMiddle(3, "TestMiddle1");
+	InsertMiddle(7, "TestMiddle2");
+
+	PrintList();
+
+	DropList();
+	PrintList();
 	return 0;
 }
