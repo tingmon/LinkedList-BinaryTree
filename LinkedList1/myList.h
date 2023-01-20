8460d7c9825215e07c6099dbf260078537f1e62d
tingmon <Tjeong2320@conestogac.on.ca>
2023-01-19 19:48:54 -0500

    partition compile done

--- LinkedList1/myList.h
+++ LinkedList1/myList.h
@@ -16,3 +16,22 @@ typedef struct LIST_INFO
 	NODE* pTail;
 	int nSize;
 } LIST_INFO;
+
+// function declare code here
+
+void InitList(LIST_INFO* pListData);
+void DropList(LIST_INFO* pListData);
+void PrintList(LIST_INFO* pListData);
+int GetLength(LIST_INFO* pListData);
+int IsEmpty(LIST_INFO* pListData);
+NODE* NodeInit(LIST_INFO* pListData, const void* data);
+NODE* GetNodeByIndex(LIST_INFO* pListData, const int index);
+NODE* GetNodeByKey(LIST_INFO* pListData, const char* key);
+int InsertAfter(LIST_INFO* pListData, NODE* pDstNode, NODE* pNewNode);
+int InsertBefore(LIST_INFO* pListData, NODE* pDstNode, NODE* pNewNode);
+int InsertHead(LIST_INFO* pListData, const void* data);
+int InsertTail(LIST_INFO* pListData, const void* data);
+int InsertMiddle(LIST_INFO* pListData, const int index, const void* data);
+int DeleteNodeByKey(LIST_INFO* pListData, const char* key);
+int DeleteNodeByIndex(LIST_INFO* pListData, const int index);
+
