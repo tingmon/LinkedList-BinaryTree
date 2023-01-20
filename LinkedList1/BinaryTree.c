69d5f82e8113c633a5bb81d7a31f12a7615f2788
tingmon <Tjeong2320@conestogac.on.ca>
2023-01-19 20:48:29 -0500

    binary insert and print

--- LinkedList1/BinaryTree.c
+++ LinkedList1/BinaryTree.c
@@ -5,42 +5,12 @@
 typedef struct NODE
 {
 	char pData[64];
-	struct NODE* left;
-	struct NODE* right;
+	struct NODE* left; 
+	struct NODE* right; 
 } NODE;
 
 NODE* g_pRoot; 
 int g_nSize;
-
-void DropTree()
-{
-	printf("g_nSize[%d], g_pRoot[%p]\n", g_nSize, g_pRoot);
-	NODE* pTemp = g_pRoot;
-	while (pTemp->next != g_pTail) {
-		NODE* pDelNode = pTemp;
-		pTemp = pTemp->next;
-		free(pDelNode);
-	}
-
-	g_pRoot = NULL;
-	g_nSize = 0;
-	// InitList();
-}
-
-void PrintTree()
-{
-	int counter = 0;
-	NODE* pTemp = g_pHead;
-	while (pTemp != NULL) {
-		printf("prev: [%p], address: [%p], next: [%p], data: %s\n", pTemp->prev, pTemp, pTemp->next, pTemp->pData);
-		pTemp = pTemp->next;
-		counter++;
-	}
-	if (counter == 0) 
-	{
-		printf("List is Empty.\n");
-	}
-}
 
 int GetLength()
 {
@@ -49,9 +19,41 @@ int GetLength()
 
 int IsEmpty()
 {
-	if (g_pHead->next == g_pTail)
+	if (g_pRoot == NULL)
 		return 1;
 	return 0;
+}
+
+//void DropTree()
+//{
+//	printf("g_nSize[%d], g_pRoot[%p]\n", g_nSize, g_pRoot);
+//	NODE* pTemp = g_pRoot;
+//	while (pTemp->next != g_pTail) {
+//		NODE* pDelNode = pTemp;
+//		pTemp = pTemp->next;
+//		free(pDelNode);
+//	}
+//
+//	g_pRoot = NULL;
+//	g_nSize = 0;
+//}
+
+void PrintTree(NODE* pParent) // pParent is likely a root node
+{
+	if (g_nSize == 0)
+	{
+		printf("List is Empty.\n");
+		return;
+	}
+	if (pParent == NULL) 
+		return;
+
+	PrintTree(pParent->left); // print this.left node
+
+	printf("this[%p], left[%p], right[%p], data: %s\n", // print this node
+		pParent, pParent->left, pParent->right, pParent->pData);
+
+	PrintTree(pParent->right); // print this.right node
 }
 
 NODE* NodeInit(const char* data)
@@ -67,20 +69,22 @@ NODE* NodeInit(const char* data)
 int InsertNode(const char* data)
 {
 	NODE* pNewNode = NodeInit(data);
+
 	if (g_pRoot == NULL) {
 		g_pRoot = pNewNode;
 		return 1;
 	}
 
 	NODE* pTemp = g_pRoot;
-	while (1) 
+	while (pTemp != NULL)
 	{
-		if (strcmp(pTemp->pData, pNewNode->pData) < 0)
+		if (strcmp(pTemp->pData, pNewNode->pData) > 0) // strcmp(criterion, value) -> criterion - value > 0 == criterion is bigger than value
 		{
 			// left
 			if (pTemp->left == NULL)
 			{
 				pTemp->left = pNewNode;
+				g_nSize++;
 				break;
 			}
 			else
@@ -95,6 +99,7 @@ int InsertNode(const char* data)
 			if (pTemp->right == NULL)
 			{
 				pTemp->right = pNewNode;
+				g_nSize++;
 				break;
 			}
 			else
@@ -107,33 +112,50 @@ int InsertNode(const char* data)
 	return g_nSize;
 }
 
-NODE* GetNodeByData(const char* data)
-{
-	NODE* pTemp = g_pHead->next;
-	while (pTemp != NULL)
-	{
-		if (strcmp(pTemp->pData, data) == 0)
-		{
-			return pTemp;
-		}
-		pTemp = pTemp->next;
-	}
-	return NULL;
-}
-
-int DeleteNodeByData(const char* data)
-{
-	NODE* pDelNode = GetNodeByData(data);
-	if (pDelNode == NULL)
-		return 0;
-	pDelNode->prev->next = pDelNode->next;
-	pDelNode->next->prev = pDelNode->prev;
-	free(pDelNode);
-	g_nSize--;
-	return 0;
-}
+//NODE* GetNodeByData(const char* data)
+//{
+//	NODE* pTemp = g_pHead->next;
+//	while (pTemp != NULL)
+//	{
+//		if (strcmp(pTemp->pData, data) == 0)
+//		{
+//			return pTemp;
+//		}
+//		pTemp = pTemp->next;
+//	}
+//	return NULL;
+//}
+//
+//int DeleteNodeByData(const char* data)
+//{
+//	NODE* pDelNode = GetNodeByData(data);
+//	if (pDelNode == NULL)
+//		return 0;
+//	pDelNode->prev->next = pDelNode->next;
+//	pDelNode->next->prev = pDelNode->prev;
+//	free(pDelNode);
+//	g_nSize--;
+//	return 0;
+//}
 
 int main() 
 {
+	InsertNode("5");
+	InsertNode("2");
+	InsertNode("6");
+	InsertNode("56");
+	InsertNode("13");
+	InsertNode("68");
+	InsertNode("94");
+	InsertNode("31");
+	InsertNode("18");
+	InsertNode("46");
+	InsertNode("33");
+	InsertNode("57");
+	InsertNode("63");
+	InsertNode("11");
+	InsertNode("24");
+
+	PrintTree(g_pRoot);
 	return 0;
 }
